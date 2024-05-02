542bcaf6fac9ea32bab9193915ec05e1d3c72640
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-05-02 23:38:49 +0530

    main

--- app.js
+++ app.js
@@ -11,14 +11,40 @@ function appendText() {
       return res.json();
     })
     .then((data) => {
-      // Object.keys(data.aboutMe).length; //check the size of JSON properties
+      console.log(data.aboutMe.name);
+      console.log(data.aboutMe.email);
+      console.log(data.aboutMe.phone);
       console.log(data.aboutMe.location);
-      //   for (var i = 0; i < Object.keys(data.aboutMe.location).length; i++) {
-      //     console.log(data.aboutMe.location[i]);
-      //   }
+
+      //for in loop
       for (var key in data) {
         console.log(data[key].education); // displays requested data
       }
+
+      console.log(data.aboutMe.portfolio);
+
+      //console.log(data.aboutMe.work); // if we need the complete  'work' key/value pair, then we can grab it like this
+
+      //traditional for loop
+      //using for loop we can iterate the list of values inside a particular key/value pair.
+      //In this case we are pulling individual values from 'work'
+      for (var i = 0; i < Object.keys(data.aboutMe.work).length; i++) {
+        console.log(data.aboutMe.work[i]);
+      }
+
+      //foreach Loop
+      const keys = Object.keys(data);
+      keys.forEach((key) => {
+        console.log(data[key].projects);
+      });
+
+      //for of Loop
+      for (var key of data.aboutMe.skills) {
+        console.log(key);
+      }
+
+      console.log(data.aboutMe.languages);
+      console.log(data.aboutMe.hobbies);
     })
 
     .catch((error) => console.error("Unable to fetch data:", error));
